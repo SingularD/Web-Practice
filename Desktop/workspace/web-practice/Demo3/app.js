5c092f58a70bce4d3bd4017bc32ec8859f242f11
SingularD <906229740@qq.com>
2018-04-05 16:40:06 +0800

    Demo3

--- Desktop/workspace/web-practice/Demo3/app.js
+++ Desktop/workspace/web-practice/Demo3/app.js
@@ -1,3 +1,27 @@
 function $(x) {
     return document.getElementById(x)
 }
+function num(a) {
+    var txt = document.getElementById("txtnum");
+    if(txt.value == "0"){
+        txt.value = a;
+    }else {
+        txt.value += a;
+    }
+    return txt.value;
+}
+function clean() {
+    $("txtnum").value = '0';
+}
+function Oper(op) {
+    var txt = document.getElementById("txtnum");
+    txt.value += op
+}
+function Decimal() {
+    var txt = document.getElementById("txtnum");
+    txt.value += '.'
+}
+function getResult() {
+    var txt = $("txtnum").value;
+    $("txtnum").value = eval(txt);
+}
