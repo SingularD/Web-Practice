f25ea41ef24aab644616e2b06348abd4d07b736d
SingularD <906229740@qq.com>
2018-04-09 16:17:08 +0800

    demo6

--- Desktop/workspace/web-practice/Demo6/app.js
+++ Desktop/workspace/web-practice/Demo6/app.js
@@ -8,24 +8,33 @@ function getOpr() {
 }
 var label = "";
 var a,b,equation;
+var x = 1,r = 1;
 function getRandom() {
      a = parseInt(Math.random()*99+1);
      b = parseInt(Math.random()*99+1);
      equation = a + getOpr() + b;
-    $("opr").innerHTML = equation + "=";
+    $("opr").innerHTML = equation + "=" ;
+    $("all").innerHTML = x;
+    x++;
+
 }
 function getNum(equation) {
 
     var result = eval(equation);
     label += equation + "<br>";
     $("show").innerHTML = label;
-    console.log(result+"getNum()");
     return result;
 }
 function submit() {
-    var myReault = $("inp").value;
+    var myReault = eval($("inp").value);
     console.log(myReault+"submit()");
     if(getNum(equation) == myReault){
         alert("回答正确！")
+        $("correct").innerHTML = r;
+        r++;
+    }else if(myReault == ""){
+        alert("请输入结果！")
+    }else {
+        alert("回答错误！")
     }
 }
