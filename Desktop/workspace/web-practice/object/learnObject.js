5469121c6c7427042515117969265bac75f01433
SingularD <906229740@qq.com>
2018-04-10 12:53:32 +0800

    object

---
+++ Desktop/workspace/web-practice/object/learnObject.js
@@ -0,0 +1,20 @@
+// var person = {
+//     name : "lisongwei",
+//     age : 22,
+//     sayName: function () {
+//         return this.name
+//     }
+// }
+// console.log(person);
+// for(var x in person){
+//     console.log(x)
+// }
+
+// var person = new Object();
+// person.name = "lisongwei";
+// person.age = 22;
+// person.sayName = function () {
+//     return person.name
+// }
+// console.log(person);
+
